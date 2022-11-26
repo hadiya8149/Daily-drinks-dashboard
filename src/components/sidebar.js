d2dad07dd79e753c14d6b0298755614e1ecab462
hadiya8149 <hadiya8149@gmail.com>
2022-11-26 12:02:54 +0500

    css update

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,12 +3,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
 import Container from "react-bootstrap/Container"
+import img from "../img/icons8-dashboard-layout-48.png"
 export default function SideBar(){
     return(
         <Navbar className="sidebar"  >
             
            <Container className="container">
-               <Navbar.Brand href="/"><h3>Daily drinks Dashboard</h3></Navbar.Brand>
+               <Navbar.Brand href="/"><h3>Daily drinks Dashboard</h3><img src={img} alt="menu-icon" className="dashboard"></img></Navbar.Brand>
             <Nav>
                 <Nav.Link className="list-group list-group-flush mx-3 mt-4 nav-item" href="/addorder">Order</Nav.Link>
             </Nav>
